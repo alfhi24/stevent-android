67ab85a2459fbe98c01b56215fd7b6b1eb8cd789
Muhammad Alfhi Saputra <alfhisa@student.telkomuniversity.ac.id>
2019-11-05 22:53:08 +0700

    Update AboutActivity.java

--- app/src/main/java/com/astronaut/stevent/AboutActivity.java
+++ app/src/main/java/com/astronaut/stevent/AboutActivity.java
@@ -1,22 +1,18 @@
 package com.astronaut.stevent;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 public class AboutActivity extends AppCompatActivity {
     private ImageView imageView;
-
     private String title = "About";
-
     private void setActionBarTitle(String title){
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle(title);
         }
     }
-
     @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
